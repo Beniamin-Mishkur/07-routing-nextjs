7a984550088a5f65558204e47346e4012446c934
Beniamin Mishkur <bmishkur@gmail.com>
2025-08-27 12:23:29 +0200

    Initial commit

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import styles from "./notes/Loading.module.css";
+
+export default function Loading() {
+  return (
+    <div className={styles.loading} role="status" aria-live="polite">
+      Loading, please wait...
+    </div>
+  );
+}
